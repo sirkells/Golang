e55d644f4e40496e455976243f012e391c5920a1
Kells <Kelechi_igbokwe@yahoo.com>
2018-04-24 10:09:15 +0200

    added some functions example

--- functions/main.go
+++ functions/main.go
@@ -15,9 +15,18 @@ type secretAgent struct {
 	person
 	licensetokill bool
 }
+type rectangle struct {
+	length float64
+	width  float64
+}
 
-// example of function
-
+//creating a func from a using a struct as an argument
+func area(r rectangle) float64 {
+	l := r.length
+	w := r.width
+	a := l * w
+	return a
+}
 func (p person) speak() {
 	fmt.Println(p.firstName, p.lastName, "good morning")
 }
@@ -25,14 +34,27 @@ func (p person) speak() {
 func (s secretAgent) speak() {
 	fmt.Println(s.firstName, s.lastName, "is hurt but alive")
 }
+
 func main() {
 
+	//calculating using the fnc and struct
+	r1 := rectangle{20, 25}
+	r2 := rectangle{30, 20}
+	fmt.Println("the area of the 1st rectangle is: ", area(r1))
+	fmt.Println("the area of the 2nd rectangle is: ", area(r2))
+
+	if area(r1) > area(r2) {
+		fmt.Println("The 1st rectangle has the largest area of ", area(r1))
+
+	} else {
+		fmt.Println("The 2nd rectangle has the largest area of ", area(r2))
+	}
+	// example of function
 	p1 := person{
 		"Kelechi",
 		"Igbokwe",
 		31,
 	}
-
 	sa1 := secretAgent{
 		person{
 			"James",
@@ -43,5 +65,4 @@ func main() {
 	}
 	sa1.speak()
 	p1.speak()
-
 }
